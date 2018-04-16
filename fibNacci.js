b76424ac234b809744ae4975e5863ecad2b91665
Cindy Kalkomey <cindy@lakesong.net>
2018-04-16 09:04:25 -0500

    JS: fib + nacci

--- fibNacci.js
+++ fibNacci.js
@@ -2,19 +2,21 @@
 // Using Closures (a closure is a function that is returned from another function) make the Fib function with the following specifications:
 
 function fib() {
-    // Some variables here
+
+    // Set starting points of current and two prev items in Fibonacci sequence
     let fibSeq = 1;
     let fibPrev1 = 0;
     let fibPrev2 = 0;
-    // There is an inner function that advances seq
+
+    // Use inner function that logs current seq value and then advances seq and two prev seq values
     function nacci() {
         console.log(fibSeq);
         // increment seq and prev
         fibPrev2 = fibPrev1;
         fibPrev1 = fibSeq;
         fibSeq = fibPrev1 + fibPrev2;
-
     }
+    //outer function returns inner function (closure)
     return nacci
   }
 
