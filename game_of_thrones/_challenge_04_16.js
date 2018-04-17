1c8eecf979e42ce34e13ba5ac3f7c9efd6d3ed13
Cindy Kalkomey <cindy@lakesong.net>
2018-04-16 21:56:43 -0500

    lecture challenge 4-16-18

---
+++ game_of_thrones/_challenge_04_16.js
@@ -0,0 +1,48 @@
+// ************ don't change between here **********
+
+function orderSupplies(item, callback) {
+    let warehouse; //undefined
+    const deliveryTime = Math.random() * 3000;
+  
+    setTimeout(function() {
+      warehouse = {
+        paint: {
+          product: 'Neon Green Paint',
+          directions: function() { return 'mix it!' }
+        },
+        brush: {
+          product: 'Horsehair brush',
+          directions: function() { return 'start painting!' }
+        }
+      };
+  
+      callback(warehouse[item]);
+    }, deliveryTime);
+  }
+  
+  // ************ and here **********
+
+
+//   function receivedItem(item) {
+//     console.log(`Received ${item.product}, time to ${item.directions() }`);
+//   }
+
+ function brushFirst(item) {
+    // console.log('calling brushFirst with item= ', item);
+    if(item.product == 'Horsehair brush'){
+        setTimeout(
+            function() {
+                // console.log('in brush Timeout');
+                console.log(`Received ${item.product}, time to ${item.directions() }`);
+            }, 
+            3000);
+    } else {
+        console.log(`Received ${item.product}, time to ${item.directions() }`);
+    }
+}
+    
+//   orderSupplies('paint', receivedItem);
+//   orderSupplies('brush', receivedItem);
+
+  orderSupplies('paint', brushFirst);
+  orderSupplies('brush', brushFirst);
