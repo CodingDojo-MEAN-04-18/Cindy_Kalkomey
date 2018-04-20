aba932d0b3330860d967f52a741e81802bb0181b
Cindy Kalkomey <cindy@lakesong.net>
2018-04-19 20:55:59 -0500

    update after code review

--- cars_and_cats/app_bak.js
+++ cars_and_cats/app_bak.js
@@ -18,47 +18,71 @@ var server = http.createServer(function (request, response){
     console.log('client request URL: ', request.url);
 
 
-    function serveContent(file, response, rspCode=200, typeContent='text/html') {
-        fs.readFile(file, function (errors, contents){
-            console.log( "error: ", errors);
-            console.log("content: ", contents);
-            response.writeHead(rspCode, {'Content-Type': typeContent});
+
+
+
+
+
+
+    
+  
+    // localhost:7077/cars go to a simple HTML page that shows some cool pictures of different cars, stored locally.
+    if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(contents); 
             response.end();
         });
     }
 
-  
-    // localhost:7077/cars go to a simple HTML page that shows some cool pictures of different cars, stored locally.
-    if(request.url === '/cars') {
-        serveContent('views/cars.html', response);
-    }
-
     // localhost:7077/cats show a simple HTML page with some cool pictures of cats, stored locally.
     else if (request.url === "/cats") {
-        serveContent('views/cats.html', response);
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+        });
     }
 
     // localhost:7077/cars/new   show a simple form where the user can add a new car information.
     else if (request.url === "/cars/new") {
-        serveContent('views/add_car.html', response);
+        fs.readFile('views/add_car.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
     }
 
     // if the stylesheet is requested
 
     else if(request.url === '/stylesheets/style.css'){
-        serveContent('./stylesheets/style.css', response,200, 'text/css');
-    }
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'text/css'});
+            response.write(contents);
+            response.end();
+        })
+      }
 
 // if an image file is requested
     else if (request.url.startsWith("/images")) {
         const imgfile = request.url.slice(8, request.url.length);
-        serveContent('./images/'+imgfile, response, 200, 'image/jpg');
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/'+imgfile, function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
     }   
+
+
 
    // If the URL is anything other than the ones above, have an error page load saying that the URL requested is not available.
     else {
-        serveContent('views/error_404.html', response, 404);
+        fs.readFile('views/error_404.html', 'utf8', function (errors, contents){
+            response.writeHead(404, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
     }
 });
 
